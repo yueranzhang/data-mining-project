9e1482465fdf8bfd32e5c09286cfc53daf114034
yueranzhang <49193854+yueranzhang@users.noreply.github.com>
2019-04-05 22:10:34 -0400

    Update keikolynn.py

--- keikolynn.py
+++ keikolynn.py
@@ -2,9 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import random
+import re
 
 time1 = time.time()
-exist_url = []
+
 
 # basic crawling function
 def crawling(link):
@@ -12,17 +13,33 @@ def crawling(link):
         headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
         r = requests.get(link, headers=headers, timeout=20)
-        html_data = BeautifulSoup(r.text, 'lxml')
+        html = r.text
+        html_data = BeautifulSoup(html, 'lxml')
         return html_data
     except Exception as e:
         print('该地址下载失败: ', link)
         print(e)
-        exist_url.append(link)
 
 
-# crawling first 10 pages
-for i in range(1, 10):
-    main_site_links = 'https://keikolynn.com/category/style/shopping-guides/page/' + str(i) + '/'
-    main_site_data = crawling(main_site_links)
-    sleep_time = random.randint(0, 2) + random.random()
-    time.sleep(sleep_time)
+# crawling first n pages
+def crawling_pages(n):
+    for i in range(1, n):
+        main_site_link = 'https://keikolynn.com/category/style/shopping-guides/page/' + str(i) + '/'
+        main_site_data = crawling(main_site_link)
+        sleep_time = random.randint(0, 2) + random.random()
+        time.sleep(sleep_time)
+
+        # get individual names from shopping guides
+        setting_names = main_site_data.findall("h2", class_="page-title")
+        set_name_list = []
+        for i in range(len(setting_names)):
+            set_name = print(setting_names[i]).a.text
+            set_name_list.append(set_name)
+            print(set_name)
+
+        # get individual links from shopping guides
+        html = crawling(main_site_link).html
+        set_links = re.findall('<h2 class="page-title"><a href="https://keikolynn.com/([^:#=<>]*?)".*?</a></h2>', html)
+        print(set_links)
+
+crawling_pages(2)
