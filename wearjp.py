a782d47b6c29adb7ddcbd5113dc2a76669bed096
yueran zhang <49193854+yueranzhang@users.noreply.github.com>
2019-04-11 18:07:42 -0400

    Create wearjp.py

---
+++ wearjp.py
@@ -0,0 +1,45 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import random
+import re
+
+time1 = time.time()
+set_link_list = []
+
+
+# basic crawling function
+def crawling(link):
+    try:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+        r = requests.get(link, headers=headers, timeout=20)
+        html = r.text
+        html_data = BeautifulSoup(html, 'lxml')
+        return html, html_data
+    except Exception as e:
+        print('该地址下载失败: ', link)
+        print(e)
+
+# crawling first n pages
+def get_sets(n):
+    for i in range(1, n):
+        main_site_link = 'https://wear.jp/coordinate/?pageno=' + str(i)
+        main_site_data = crawling(main_site_link)[1]
+
+        # get individual links from shopping guides
+        set_imgs = main_site_data.find_all(name="div",attrs={"class":"image"})
+        for set_img in set_imgs:
+            set_link_text = set_img.a
+            set_link = [l['href'] for l in set_link_text]
+            set_link_list.append(set_link)
+
+        # sleep
+        sleep_time = random.randint(0, 1) + random.random()
+        time.sleep(sleep_time)
+
+get_sets(3)
+time2 = time.time()
+total_time = time2 - time1
+print(set_link_list)
+print(total_time)
