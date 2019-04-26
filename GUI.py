74ae88fab51dbcbb6afb36295301ef92a442ffee
yueran zhang <49193854+yueranzhang@users.noreply.github.com>
2019-04-25 21:23:05 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -10,41 +10,41 @@ class GUI():
         self.tk.geometry('800x600')
         # 右侧图片
         self.logo = PhotoImage(file='image1.gif')
-        self.textLabel = Label(self.tk, image=self.logo)
-        self.textLabel.place(x=100, y=0)
+        textLabel = Label(self.tk, image=self.logo)
+        textLabel.place(x=100, y=0)
 
         # 输入lable
-        self.input_label = Label(self.tk, text='Please input a file path', bg='sky blue', font=(44))
-        self.input_label.place(x=110, y=300)
+        input_label = Label(self.tk, text='Please input a file path', bg='sky blue', font=(44))
+        input_label.place(x=110, y=300)
 
         # 文件路径entry
-        self.path_entry = Entry(self.tk)
-        self.path_entry.place(x=300, y=300)
+        path_entry = Entry(self.tk)
+        path_entry.place(x=300, y=300)
 
         # upload按钮
-        self.upload_button = Button(self.tk, text='upload', bg='sky blue', command=self.input_window)
-        self.upload_button.place(x=500, y=300)
+        upload_button = Button(self.tk, text='upload', bg='sky blue', command=self.input_window)
+        upload_button.place(x=500, y=300)
 
         # show按钮
-        self.show_button = Button(self.tk, text='show', bg='sky blue', command=self.show_window)
-        self.show_button.place(x=500, y=350)
+        show_button = Button(self.tk, text='show', bg='sky blue', command=self.show_window)
+        show_button.place(x=500, y=350)
 
         # 输入最下方文本
-        self.text = Text(self.tk, height=2)
-        self.text.place(x=100, y=500)
-        self.text.insert(END, 'This tool can help you to match cloths based on your input images. Input: upload image. '
+        text = Text(self.tk, height=2)
+        text.place(x=100, y=500)
+        text.insert(END, 'This tool can help you to match cloths based on your input images. Input: upload image. '
                               'Output: output image')
 
     def start(self):
         self.tk.mainloop()
 
     def show_window(self):
-        self.s_window = Toplevel()
-        self.s_window.title('Result')
+        s_window = Toplevel()
+        s_window.title('Result')
 
     def input_window(self):
-        self.i_window = Toplevel()
-        self.i_window.title('input path')
+        i_window = Toplevel()
+        i_window.title('input path')
 
 
 if __name__ == '__main__':
