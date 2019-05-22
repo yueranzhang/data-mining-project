3660e639f187d08d7ef90421204449dfc4f4a75f
yueran zhang <49193854+yueranzhang@users.noreply.github.com>
2019-05-22 19:28:36 -0400

    Update and rename GUI.py to Indico_GUI.py

--- Indico_GUI.py
+++ Indico_GUI.py
@@ -3,39 +3,43 @@ import cv2 as cv
 import indicoio
 from indicoio.custom import Collection
 from PIL import Image,ImageTk
+from shopping_link import *
+from tkinter.filedialog import askopenfilename
 
 class GUI():
     def __init__(self):
-        self.tk = Tk()  # 实例化
-        # 标题
-        self.tk.title('clothing matching system')
-        # 大小
+        self.tk = Tk()
+        # title
+        self.tk.title('Indico')
+        # size
         self.tk.geometry('800x600')
-        # 右侧图片
+        # img on the right
         self.logo = PhotoImage(file='image1.gif')
         textLabel = Label(self.tk, image=self.logo)
         textLabel.place(x=100, y=0)
 
-        # 输入lable
+        # entry lable
         input_label = Label(self.tk, text='Please input a file path', bg='sky blue', font=(44))
         input_label.place(x=110, y=300)
 
-        # 文件路径entry
+        # file entry
         self.path_entry = Entry(self.tk)
         self.path_entry.place(x=300, y=300)
-        # upload按钮
-        upload_button = Button(self.tk, text='upload', bg='sky blue')
+        # upload button
+        upload_button = Button(self.tk, text='upload', bg='sky blue', command = self.selectPath)
         upload_button.place(x=500, y=300)
+        self.path = StringVar()
 
-        # show1按钮
+
+        # show1
         show_button_1 = Button(self.tk, text='cloth-pants', bg='sky blue', command=self.show_window_1)
         show_button_1.place(x=500, y=350)
 
-        # show2按钮
+        # show2
         show_button_2 = Button(self.tk, text='cloth-shoes', bg='sky blue', command=self.show_window_2)
         show_button_2.place(x=500, y=400)
 
-        # 输入最下方文本
+        # text in the bttom
         text = Text(self.tk, height=2)
         text.place(x=100, y=500)
         text.insert(END, 'This tool can help you to match cloths based on your input images. Input: upload image. '
@@ -50,36 +54,53 @@ class GUI():
     def show_window_1(self):
         # indico
         indicoio.config.api_key = 'bb2ce29a2086bceb98bc297f6a961656'
-        img = cv.imread("/Users/yueranzhang/Desktop/"+self.path_entry.get())
+        img = cv.imread(self.path_entry.get())
         collection = Collection("clothes_collection_2")
         predict = collection.predict(img)
         outcome = max(predict, key=predict.get)
         outcome = outcome[5:]
-        # 输出结果的图片
+        print(outcome)
+        # result img
         result_im = Image.open("my_training_shirts/"+outcome+".jpg")
         self.result_img = ImageTk.PhotoImage(result_im)
 
         s_window = Toplevel()
+        s_window.geometry('600x400')
         s_window.title('Result')
         Label(s_window, text='your result is:').place(x=10,y=10)
         Label(s_window, image=self.result_img).place(x=10,y=30)
+        Label(s_window, text='The link is:').place(x=200, y=10)
+        dic = get_link()
+        link = dic[outcome + '.jpg']
+        Label(s_window, text=link).place(x=200, y=30)
 
     def show_window_2(self):
         # indico
         indicoio.config.api_key = 'bb2ce29a2086bceb98bc297f6a961656'
-        img = cv.imread("/Users/yueranzhang/Desktop/"+self.path_entry.get())
+        img = cv.imread(self.path_entry.get())
         collection = Collection("clothes_collection_1")
         predict = collection.predict(img)
         outcome = max(predict, key=predict.get)
         outcome = outcome[5:]
-        # 输出结果的图片
+        # result img
         result_im = Image.open("my_training_shirts/"+outcome+".jpg")
         self.result_img = ImageTk.PhotoImage(result_im)
 
         s_window = Toplevel()
+        s_window.geometry('600x400')
         s_window.title('Result')
         Label(s_window, text='your result is:').place(x=10,y=10)
         Label(s_window, image=self.result_img).place(x=10,y=30)
+        Label(s_window, text='The link is:').place(x=200, y=10)
+        dic = get_link()
+        link = dic[outcome+'.jpg']
+        Label(s_window, text=link).place(x=200, y=30)
+
+    def selectPath(self):
+        path_ = askopenfilename()
+        self.path.set(path_)
+        self.path_entry.delete(0,'end')
+        self.path_entry.insert(END,str(path_))
 
 
 
